287f34ec35e7ac5ee2034801b1a8cbf61f4cd4bd
SSep <ssep4u@gmail.com>
2019-04-04 19:55:44 +0900

    add Nsum project

---
+++ 20190404_Nsum/src/kr/hs/emirim/nsum/Nsum.java
@@ -0,0 +1,39 @@
+package kr.hs.emirim.nsum;
+
+import java.util.Scanner;
+
+public class Nsum {
+
+	public static void main(String[] args) {
+		//숫자 String으로 입력받자 <= 자릿수를 알기 위해서 <= 0~자릿수-1까지 하나씩 잘라서 더해야함
+		Scanner scanner = new Scanner(System.in);
+		String number = scanner.nextLine();
+		//각 자릿수의 수를 더하자
+		int length = number.length();
+		int sum = 0;
+		for(int i = 0; i< length; i++) {
+			//0~자릿수-1까지 반복하자
+			//  한글자씩 가져와서 숫자로 바꾸자
+			char n = number.charAt(i);
+			//  sum에 더하자
+			sum += n - '0';
+		}
+		//더한 값을 출력하자
+		System.out.println(sum);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
