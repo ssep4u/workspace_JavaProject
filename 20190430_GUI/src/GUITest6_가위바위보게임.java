7c231b746b23f309454523d2a0a93cfe0bc3f6a8
SSep <ssep4u@gmail.com>
2019-05-23 18:53:21 +0900

    귀여운 가위바위보 그림 추가

--- 20190430_GUI/src/GUITest6_가위바위보게임.java
+++ 20190430_GUI/src/GUITest6_가위바위보게임.java
@@ -8,6 +8,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class GUITest6_가위바위보게임 {
+	static String[] filename = {"src/images/scissors.png", "src/images/rock.png", "src/images/paper.png"};
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("가위바위보 게임");
@@ -16,15 +17,15 @@ public class GUITest6_가위바위보게임 {
 		
 		//Component 만들자
 		JLabel result = new JLabel("Win");
-		ImageIcon computerImage = new ImageIcon("");
-		JLabel computerLabel = new JLabel(computerImage);
+		ImageIcon computerImage = new ImageIcon(filename[1]);
+		JLabel computerLabel = new JLabel(computerImage);		//컴퓨터 패의 그림
 		
-		ImageIcon scissorsImage = new ImageIcon("");
-		JButton scissorsButton = new JButton(scissorsImage);
-		ImageIcon rockImage = new ImageIcon("");
-		JButton rockButton = new JButton(rockImage);
-		ImageIcon paperImage = new ImageIcon("");
-		JButton paperButton = new JButton(paperImage);
+		ImageIcon scissorsImage = new ImageIcon(filename[0]);
+		JButton scissorsButton = new JButton(scissorsImage);	//가위 버튼
+		ImageIcon rockImage = new ImageIcon(filename[1]);
+		JButton rockButton = new JButton(rockImage);			//바위 버튼
+		ImageIcon paperImage = new ImageIcon(filename[2]);
+		JButton paperButton = new JButton(paperImage);			//보 버튼
 		
 		panel.add(result);
 		panel.add(computerLabel);
