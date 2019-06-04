8b7392bf2c777490f295e22dab08ec35df77205d
SSep <ssep4u@gmail.com>
2019-06-04 19:49:49 +0900

    우와 대박 GUI 업앤다운

---
+++ 20190430_GUI/src/GUITest11_UpDown.java
@@ -0,0 +1,76 @@
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class GUITest11_UpDown {
+	static int computer;
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("업앤다운업앤다운");
+		JPanel panel = new JPanel();
+		frame.setPreferredSize(new Dimension(400, 600));
+		
+		//JTextField, JButton
+		JTextField tfInput = new JTextField(10);
+		JButton btQuestion = new JButton("이냐?");
+		//JLabel
+		JLabel lbResult = new JLabel("1~100사이 숫자 맞춰봐");
+		Random random = new Random();
+		computer = random.nextInt(100) + 1; // 1~100
+		ActionListener al = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//사용자입력값 가져오자
+				String input = tfInput.getText();
+				int player = Integer.parseInt(input);	//String -> int
+				//컴퓨터값이랑 비교하자
+				//결과 보여주자
+				if (computer > player) {
+					lbResult.setText("Up");
+				} else if (computer < player) {
+					lbResult.setText("Down");
+				} else {
+					// 결과
+					// ==탈출
+					lbResult.setText("정답입니다.");
+					computer = random.nextInt(100) + 1;
+				}
+			}
+		};
+		btQuestion.addActionListener(al);
+		
+		panel.add(tfInput);
+		panel.add(btQuestion);
+		panel.add(lbResult);
+		frame.add(panel);
+		
+		frame.pack();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
