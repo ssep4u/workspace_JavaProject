2f02db88b99ff82f09344572cf461edd6e3d7f5d
SSep <ssep4u@gmail.com>
2019-06-04 19:15:45 +0900

    우와 대박 GUI 가위바위보

--- 20190430_GUI/src/GUITest6_가위바위보게임.java
+++ 20190430_GUI/src/GUITest6_가위바위보게임.java
@@ -1,5 +1,8 @@
 import java.awt.Dimension;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -9,6 +12,10 @@ import javax.swing.JPanel;
 
 public class GUITest6_가위바위보게임 {
 	static String[] filename = {"src/images/scissors.png", "src/images/rock.png", "src/images/paper.png"};
+	static String[] answerString = {"사용자가 짐", "비김", "사용자가 이김 ^^"};
+	protected static int computer;
+	protected static int player;
+	protected static int answer;
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("가위바위보 게임");
@@ -26,6 +33,31 @@ public class GUITest6_가위바위보게임 {
 		JButton rockButton = new JButton(rockImage);			//바위 버튼
 		ImageIcon paperImage = new ImageIcon(filename[2]);
 		JButton paperButton = new JButton(paperImage);			//보 버튼
+		ActionListener al = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//1:가위, 2:바위, 3:보
+				if(e.getSource() == scissorsButton) {
+					player = 1;
+				} else if(e.getSource() == rockButton) {
+					player = 2;
+				} else if(e.getSource() == paperButton) {
+					player = 3;
+				}
+				//makeComputer
+				computer = makeComputer();
+				ImageIcon computerImage = new ImageIcon(filename[computer-1]);
+				computerLabel.setIcon(computerImage);
+				//compare
+				answer = compare(player, computer);	//0:내가 짐, 1:비김, 2:내가 이김
+				//결과를 화면에 보여주자
+				result.setText(answerString[answer]);
+			}
+		};
+		scissorsButton.addActionListener(al);
+		rockButton.addActionListener(al);
+		paperButton.addActionListener(al);
+		
 		
 		panel.add(result);
 		panel.add(computerLabel);
@@ -37,6 +69,26 @@ public class GUITest6_가위바위보게임 {
 		frame.pack();
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	private static int compare(int p, int c) {
+//		if((p == 1 && c == 2) || (p == 2 && c == 3) || (p == 3 && c == 1)) {
+		if ((p + 1) % 3 == c % 3) {
+			// 0: 사용자가 짐
+			return 0;
+//		} else if((p == 1 && c == 1) || (p == 2 && c == 2) || (p == 3 && c == 3)) {
+		} else if (p == c) {
+			// 1: 비김
+			return 1;
+		} else {
+			// 2: 사용자가 이김
+			return 2;
+		}
+	}
+
+	private static int makeComputer() {
+		Random random = new Random();
+		int c = random.nextInt(3 - 1 + 1) + 1; // 1~3 임의의 수
+		return c;
 	}
 
 }
